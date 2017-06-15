d9abce708670f3e8934e46351b2b6c514b0175ce
Yann Hamon <yann.hamon@9cookies.com>
2017-06-15 17:29:47 +0200

    Add gitignore & go fmt files

--- main.go
+++ main.go
@@ -8,58 +8,59 @@ import "github.com/docopt/docopt-go"
 
 type PuppetModule interface {
 	Name() string
-  Download() string
-  SetTargetFolder(string)
-  TargetFolder() string
+	Download() string
+	SetTargetFolder(string)
+	TargetFolder() string
 }
 
 type Parser interface {
-  parse(puppetFile io.Reader, modulesChan chan PuppetModule, wg *sync.WaitGroup) error
+	parse(puppetFile io.Reader, modulesChan chan PuppetModule, wg *sync.WaitGroup) error
 }
 
 func cloneWorker(c chan PuppetModule, wg *sync.WaitGroup) {
-  parser := MetadataParser{}
+	parser := MetadataParser{}
 
-  for m := range c{
-    target :=m.Download()
+	for m := range c {
+		target := m.Download()
 
-    if file, err := os.Open(target+"/metadata.json"); err == nil {
-      defer file.Close()
-     	parser.parse(file, c, wg)
-    }
+		if file, err := os.Open(target + "/metadata.json"); err == nil {
+			defer file.Close()
+			wg.Add(1)
+			go parser.parse(file, c, wg)
+		}
 
-    wg.Done()
-  }
+		wg.Done()
+	}
 }
 
 func main() {
-  usage := `r10k-go.
+	usage := `r10k-go.
 
 Usage:
   r10k-go deploy environment <env_name>
 
 Options:
   -h --help     Show this screen.`
-  numWorkers := 2
+	numWorkers := 2
 
-  docopt.Parse(usage, nil, true, "r10k-go", false)
+	docopt.Parse(usage, nil, true, "r10k-go", false)
 
-  file, err := os.Open("Puppetfile")
-  if err != nil {
-      log.Fatal(err)
-  }
-  defer file.Close()
+	file, err := os.Open("Puppetfile")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-  modulesChan := make(chan PuppetModule)
-  var wg sync.WaitGroup
+	modulesChan := make(chan PuppetModule)
+	var wg sync.WaitGroup
 
-  for w := 1; w <= numWorkers; w++ {
-    go cloneWorker(modulesChan, &wg)
-  }
+	for w := 1; w <= numWorkers; w++ {
+		go cloneWorker(modulesChan, &wg)
+	}
 
-  parser := PuppetFileParser{}
+	parser := PuppetFileParser{}
 	parser.parse(file, modulesChan, &wg)
 
-  wg.Wait()
-  close(modulesChan)
+	wg.Wait()
+	close(modulesChan)
 }
--- parser_metadata.go
+++ parser_metadata.go
@@ -1,4 +1,3 @@
-
 package main
 
 import "io"
@@ -6,28 +5,30 @@ import "encoding/json"
 import "io/ioutil"
 import "sync"
 
-type Metadata struct{
-  Name string
-  Dependencies []struct {
-    Name string
-    Version_requirement string
-  }
+type Metadata struct {
+	Name         string
+	Dependencies []struct {
+		Name                string
+		Version_requirement string
+	}
 }
 
 type MetadataParser struct {
 }
 
 func (p MetadataParser) parse(r io.Reader, modulesChan chan PuppetModule, wg *sync.WaitGroup) error {
-  var m Metadata
-  metadataFile, _ := ioutil.ReadAll(r)
+	var m Metadata
 
-  json.Unmarshal(metadataFile, &m)
-  for _, req := range m.Dependencies {
-    wg.Add(1)
-    modulesChan <- &ForgeModule{name: req.Name, version_requirement: req.Version_requirement}
-  }
+	defer wg.Done()
+	metadataFile, _ := ioutil.ReadAll(r)
 
-  return nil
+	json.Unmarshal(metadataFile, &m)
+	for _, req := range m.Dependencies {
+		wg.Add(1)
+		modulesChan <- &ForgeModule{name: req.Name, version_requirement: req.Version_requirement}
+	}
+
+	return nil
 }
 
 // func main() {
