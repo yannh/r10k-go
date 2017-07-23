eb5fc3b5a8dc61100f8db756c59a58f196070bd5
Yann Hamon <yann@mandragor.org>
2017-07-23 13:40:35 +0200

    Fixed doctopt string, thought options where global parameters...

--- cli.go
+++ cli.go
@@ -1,21 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"github.com/docopt/docopt-go"
 )
 
 func cli() map[string]interface{} {
 
-	usage := `librarian-go
+	usage := `r10k-go
 
-  Usage:
-    r10k-go install [--puppetfile=<PUPPETFILE>] [--workers=<workers>] [--no-deps]
-    r10k-go deploy environment <ENV> [--puppetfile=<PUPPETFILE>] [--workers=<workers>]
+Usage:
+  r10k-go install [--modulePath=<PATH>] [--puppetfile=<PUPPETFILE>] [--workers=<n>] [--no-deps]
+  r10k-go deploy environment <ENV> [--puppetfile=<PUPPETFILE>] [--workers=<WORKERS>]
+  r10k-go -h | --help
+  r10k-go --version
 
-  Options:
-    --modulesPath=<PATH>     Path to the modules folder
-    -h --help                Show this screen.`
+Options:
+  --modulesPath=<PATH>        Path to the modules folder
+  --no-deps                   Skip downloading modules dependencies
+  --puppetFile=<PUPPETFILE>   Path to the modules folder
+  --workers=<n>               Number of modules to download in parallel
+  -h --help                   Show this screen.
+`
 
-	opts, _ := docopt.Parse(usage, nil, true, "r10k-go", false)
+	opts, _ := docopt.Parse(usage, nil, true, "0.1", true)
+	fmt.Println(opts)
 	return opts
 }
