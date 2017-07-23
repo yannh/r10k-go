f81949fa09802b051d033ba6f9a081b31d31956f
Yann Hamon <yann.hamon@9cookies.com>
2017-07-23 13:27:48 +0200

    Added option to skip downloading deps.. having issues with docopt options :/

--- cli.go
+++ cli.go
@@ -1,13 +1,15 @@
 package main
 
-import "github.com/docopt/docopt-go"
+import (
+	"github.com/docopt/docopt-go"
+)
 
 func cli() map[string]interface{} {
 
 	usage := `librarian-go
 
   Usage:
-    r10k-go install [--puppetfile=<PUPPETFILE>] [--workers=<workers>]
+    r10k-go install [--puppetfile=<PUPPETFILE>] [--workers=<workers>] [--no-deps]
     r10k-go deploy environment <ENV> [--puppetfile=<PUPPETFILE>] [--workers=<workers>]
 
   Options:
@@ -15,6 +17,5 @@ func cli() map[string]interface{} {
     -h --help                Show this screen.`
 
 	opts, _ := docopt.Parse(usage, nil, true, "r10k-go", false)
-
 	return opts
 }
