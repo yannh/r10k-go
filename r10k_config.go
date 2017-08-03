18c431728ecaed35bb393ac566445fd473346fa9
Yann Hamon <yann@mandragor.org>
2017-08-03 23:53:28 +0200

    Initial support for r10k environments.. getting there!

---
+++ r10k_config.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bytes"
+	"gopkg.in/yaml.v2"
+	"io"
+	"os"
+	"log"
+)
+
+type source struct {
+	Basedir string
+	Prefix  string
+	Remote  string
+}
+
+type r10kConfig struct {
+	Cachedir string
+	Sources  map[string]source
+}
+
+func NewR10kConfig(filename string) (*r10kConfig, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("could not open %s: %v", filename, err)
+	}
+
+	return parseR10kConfig(f)
+}
+
+func parseR10kConfig(r io.Reader) (*r10kConfig, error) {
+	c := &r10kConfig{}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r)
+
+	err := yaml.Unmarshal(buf.Bytes(), c)
+  if err != nil {
+    return nil, err
+  }
+
+	return c, nil
+}
